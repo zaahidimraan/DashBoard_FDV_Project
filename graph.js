e33deb1646a82f4779f54940a9c6697b02adbc72
Zahid Imran <86535928+zahid479833@users.noreply.github.com>
2022-12-03 15:59:19 +0500

    File reading Done

--- graph.js
+++ graph.js
@@ -1,11 +1,13 @@
 
-const data = await d3.json("./flare.json");
+//const data = await d3.json("./flare.json");
+var data = await d3.csv("./birdstrikes.csv");
+  
 let clickme1= document.getElementById('clickmeQ1');
 clickme1.onclick=function(){
     console.log("\nData Output from file\n");
     console.log(data);
-    let root=d3.hierarchy(data);
-    console.log("\nData Output from using d3.heirarchy\n");
-    console.log(root);
+ //   let root=d3.hierarchy(data);
+ //   console.log("\nData Output from using d3.heirarchy\n");
+ //   console.log(root);
 }
 
