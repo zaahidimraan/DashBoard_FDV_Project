3a619aecfc110bee6d6c173b112aa5c559243024
Zahid Imran <86535928+zahid479833@users.noreply.github.com>
2022-12-03 22:47:00 +0500

    BarPlot with dates

--- graph.js
+++ graph.js
@@ -17,14 +17,11 @@ clickme1.onclick=function(){
 
 let clickme2= document.getElementById('clickmeQ2');
 clickme2.onclick=function(){
-    console.log("\nData Output from file\n");
-    console.log(data);
-   let root=d3.hierarchy(data);
-    console.log("\nData Output from using d3.heirarchy\n");
+    let root=d3.hierarchy(data);
     let arrayData1=storeInArray(root);
     let arrayData=arrayData1.slice(1,10);
-// set the dimensions and margins of the graph
-var margin = {top: 10, right: 30, bottom: 90, left: 40},
+   // set the dimensions and margins of the graph
+   var margin = {top: 10, right: 30, bottom: 90, left: 40},
     width = 460 - margin.left - margin.right,
     height = 450 - margin.top - margin.bottom;
 
@@ -40,7 +37,7 @@ var svg = d3.select("#my_dataviz")
 // Barplot
   var x = d3.scaleBand()
    .range([ 0, width ])
-   .domain(arrayData.map(function(d) { return d.airportName; }))
+   .domain(arrayData.map(function(d) { return d.flightDate; }))
    .padding(0.2);
   svg.append("g")
    .attr("transform", "translate(0," + height + ")")
@@ -51,17 +48,50 @@ var svg = d3.select("#my_dataviz")
 
 // Add Y axis
 var y = d3.scaleLinear()
-  .domain([0, 500])
+  .domain([0, 300])
   .range([ height, 0]);
 svg.append("g")
   .call(d3.axisLeft(y));
+
+  // create a tooltip
+  var Tooltip = d3.select("#my_dataviz")
+    .append("div")
+    .style("opacity", 0)
+    .attr("class", "tooltip")
+    .style("background-color", "white")
+    .style("border", "solid")
+    .style("border-width", "2px")
+    .style("border-radius", "5px")
+    .style("padding", "5px")
+
+  // Three function that change the tooltip when user hover / move / leave a cell
+  var mouseover = function(d) {
+    Tooltip
+      .style("opacity", 1)
+    d3.select(this)
+      .style("stroke", "black")
+      .style("opacity", 1)
+  }
+  var mousemove = function(d) {
+    Tooltip
+      .html("The exact value of<br>this cell is: " + d.airportName)
+      .style("left", (d3.mouse(this)[0]+70) + "px")
+      .style("top", (d3.mouse(this)[1]) + "px")
+  }
+  var mouseleave = function(d) {
+    Tooltip
+      .style("opacity", 0)
+    d3.select(this)
+      .style("stroke", "none")
+      .style("opacity", 0.8)
+  }
 
 // Bars
 svg.selectAll("mybar")
   .data(arrayData)
   .enter()
   .append("rect")
-    .attr("x", function(d) { return x(d.airportName); })
+    .attr("x", function(d) { return x(d.flightDate); })
     .attr("width", x.bandwidth())
     .attr("fill", "#69b3a2")
     // no bar at the beginning thus:
@@ -75,6 +105,9 @@ svg.selectAll("rect")
   .attr("y", function(d) { return y(d.speedIASinKnots); })
   .attr("height", function(d) { return height - y(d.speedIASinKnots); })
   .delay(function(d,i){console.log(i) ; return(i*100)})
+  .on("mouseover", mouseover)
+  .on("mousemove", mousemove)
+  .on("mouseleave", mouseleave)
 
 }
 
