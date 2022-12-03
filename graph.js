dbcd9370cbcf9c399be718972e33c7c00627a22a
Zahid Imran <86535928+zahid479833@users.noreply.github.com>
2022-12-04 03:36:13 +0500

    Month Base

--- graph.js
+++ graph.js
@@ -127,8 +127,8 @@ clickme3.onclick=function(){
     
     console.log(sets);
     // set the dimensions and margins of the graph
-   var margin = {top: 10, right: 30, bottom: 30, left: 60},
-   width = 460 - margin.left - margin.right,
+   var margin = {top: 10, right: 30, bottom: 60, left: 60},
+   width = 1200 - margin.left - margin.right,
    height = 450 - margin.top - margin.bottom;
 
   // append the svg object to the body of the page
@@ -154,19 +154,70 @@ clickme3.onclick=function(){
      .style("text-anchor", "end");
 
 // Add Y axis
-  var y = d3.scaleBand()
-  .range([ height, 0 ])
-  .domain(arrayData.map(function(d) { if(d.flightDate[1]=='/')
-                                           return d.flightDate[0]
+   var y = d3.scaleBand()
+    .range([ height, 0 ])
+    .domain(arrayData.map(function(d) { if(d.flightDate[1]=='/')
+                                           return d.flightDate[0];
                                        else
-                                           return `${d.flightDate[0]}${d.flightDate[1]}`
-                                    ; }))
-  .padding(0.5);
- svg.append("g")
-  .call(d3.axisLeft(y))
-  .selectAll("text")
-   .style("text-anchor", "end");
+                                           return `${d.flightDate[0]}${d.flightDate[1]}`;
+                                     }))
+    .padding(0.5);
+   svg.append("g")
+    .call(d3.axisLeft(y))
+    .selectAll("text")
+     .style("text-anchor", "end");
 
+   // Add a tooltip div. Here I define the general feature of the tooltip: stuff that do not depend on the data point.
+// Its opacity is set to 0: we don't see it by default.
+  var tooltip = d3.select("#my_dataviz1")
+   .append("div")
+   .style("opacity", 0)
+   .attr("class", "tooltip")
+   .style("background-color", "white")
+   .style("border", "solid")
+   .style("border-width", "1px")
+   .style("border-radius", "5px")
+   .style("padding", "10px")
+
+
+
+// A function that change this tooltip when the user hover a point.
+// Its opacity is set to 1: we can now see it. Plus it set the text and position of tooltip depending on the datapoint (d)
+  var mouseover = function(d) {
+   tooltip
+    .style("opacity", 1)
+  }
+
+  var mousemove = function(d) {
+   tooltip
+    .html("The exact value of<br>the Ground Living area is: " + d.originState)
+    .style("left", (d3.mouse(this)[0]+90) + "px") // It is important to put the +90: other wise the tooltip is exactly where the point is an it creates a weird effect
+    .style("top", (d3.mouse(this)[1]) + "px")
+  }
+
+  // A function that change this tooltip when the leaves a point: just need to set opacity to 0 again
+  var mouseleave = function(d) {
+   tooltip
+     .transition()
+     .duration(200)
+     .style("opacity", 0)
+   }
+
+// Add dots
+svg.append('g')
+.selectAll("dot")
+.data(arrayData) 
+.enter()
+.append("circle")
+  .attr("cx", function (d) { return x(d.originState); } )
+  .attr("cy", function (d) { return y(d.flightDate[1]=='/'?d.flightDate[0]:`${d.flightDate[0]}${d.flightDate[1]}`); } )
+  .attr("r", function(d){ return (d.WildlifeSize=='Large'?15:(d.WildlifeSize=='Medium'?7:2))})
+  .style("fill", "#69b3a2")
+  .style("opacity", 0.3)
+  .style("stroke", "white")
+.on("mouseover", mouseover )
+.on("mousemove", mousemove )
+.on("mouseleave", mouseleave )
 
 }
 
