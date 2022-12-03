bc23136d6875cef8530afb49ab522cb7302ff0c9
Zahid Imran <86535928+zahid479833@users.noreply.github.com>
2022-12-03 16:28:18 +0500

    Understanding Data Flow

--- graph.js
+++ graph.js
@@ -6,8 +6,8 @@ let clickme1= document.getElementById('clickmeQ1');
 clickme1.onclick=function(){
     console.log("\nData Output from file\n");
     console.log(data);
- //   let root=d3.hierarchy(data);
- //   console.log("\nData Output from using d3.heirarchy\n");
- //   console.log(root);
+   let root=d3.hierarchy(data);
+    console.log("\nData Output from using d3.heirarchy\n");
+    console.log(root.data[0]['Airport Name']);
 }
 
