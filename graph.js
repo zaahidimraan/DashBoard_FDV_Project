a88c1d3c846483f0858af48224f8e2d7384b1d6c
Zahid Imran <86535928+zahid479833@users.noreply.github.com>
2022-12-04 17:58:08 +0500

    slider-range added

--- graph.js
+++ graph.js
@@ -1,3 +1,18 @@
+$(function () {
+  $("#slider-range").slider({
+      range: true,
+      min: new Date('1990.01.01').getTime() / 1000,
+      max: new Date('1990.12.01').getTime() / 1000,
+      step: 86400,
+      values: [new Date('1990.02.01').getTime() / 1000, new Date('1990.02.01').getTime() / 1000],
+      slide: function (event, ui) {
+          $("#amount").val((new Date(ui.values[0] * 1000).toDateString()) + " - " + (new Date(ui.values[1] * 1000)).toDateString());
+      }
+  });
+  $("#amount").val((new Date($("#slider-range").slider("values", 0) * 1000).toDateString()) +
+      " - " + (new Date($("#slider-range").slider("values", 1) * 1000)).toDateString());
+});
+
 
 var data = await d3.csv("./birdstrikes.csv");
   
@@ -116,6 +131,8 @@ svg.selectAll("rect")
 
 let clickme3= document.getElementById('clickmeQ3');
 clickme3.onclick=function(){
+   
+   console.log(d3.select("#slider-range").values);
    let root=d3.hierarchy(data);
    let arrayData=[];
     // Storing in an array 
